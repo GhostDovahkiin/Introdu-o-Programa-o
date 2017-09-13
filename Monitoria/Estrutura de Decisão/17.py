869b04da98436be09a8cb926b86dfd29aaf52c3d
Pedro Henrique <hearthfire@inbox.ru>
2017-09-13 14:05:54 -0300

    Att

---
+++ Monitoria/Estrutura de Decisão/17.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto
+ano = int(input("Digite um ano: "))
+if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
+    print("{} é um ano bissexto" .format(ano))
+else:
+    print("{} não é um ano bissexto" .format(ano))
