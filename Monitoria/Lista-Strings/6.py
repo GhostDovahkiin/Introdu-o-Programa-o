814bc8fb1efcb43ad445ab3820382a33604a8a82
Pedro Henrique <hearthfire@inbox.ru>
2017-10-14 00:51:19 -0300

    1.1

--- Monitoria/Lista-Strings/6.py
+++ Monitoria/Lista-Strings/6.py
@@ -1,4 +1,4 @@
-dia, mes, ano = map(str,input(":").split("/"))
+dia, mes, ano = map(str,input("Digite o dia, mês e ano de nascimento. \nExemplo: (xx/xx/xxxx)\n ----> ").split("/"))
 meses = [" ", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
 mes = meses[int(mes)]
 print("Você nasceu em {} de {} de {}" .format(dia, mes, ano))
--- Monitoria/Lista-Strings/8.py
+++ Monitoria/Lista-Strings/8.py
@@ -1,8 +1,12 @@
 print("PALÍNDROMO")
 str1 = list(str.lower(input("Digite uma palavra, ou frase: ")))
-str1.remove(" ") 
-str2 = str1[::-1]   
-if str1 == str2:
+for i in str1:
+        if i == " ":
+            str1.remove(" ")
+            print(str1)
+        else:
+            break
+if str1 == str1[::-1] :
     print("É UM PALÍNDROMO")
 else:
     print("NÃO É UM PALÍNDROMO")
